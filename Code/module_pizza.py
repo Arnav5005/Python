6b03de50a4873662d0e8ea2b2ef2ae8ba982c84d
Arnav pratap singh <arnavpratapsingh2006@gmail.com>
2026-03-19 00:49:00 +0530

    moving files to new folder

--- Code/module_pizza.py
+++
@@ -1,3 +0,0 @@
-import pizza
-pizza.make_pizza(16,'pepperoni')
-pizza.make_pizza(18,'capsicum' , 'tomato' , 'onion' , 'corn')
--- Code/test function.py
+++
@@ -1,9 +0,0 @@
-import pygame
-
-screen = pygame.display.set_mode((1278,654))
-
-running = True
-while running :
-    for event in pygame.event.get() :
-        if event.type == pygame.QUIT :
-            running = False
